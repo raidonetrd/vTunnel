376846805660e46d150ca5c393713f76a2c48fe8
Alex Tsai <alex.tsai@protonmail.com>
2021-04-29 11:57:00 +0800

    add DohResolver

--- app/src/test/java/com/netbyte/vtun/ExampleUnitTest.java
+++ app/src/test/java/com/netbyte/vtun/ExampleUnitTest.java
@@ -1,11 +1,22 @@
 package com.netbyte.vtun;
 
 import org.junit.Test;
+import org.xbill.DNS.DClass;
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Name;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Section;
+import org.xbill.DNS.Type;
+
+import java.io.IOException;
 
 public class ExampleUnitTest {
     @Test
-    public void test() {
-
+    public void test() throws IOException {
+        String host = "https://1.1.1.1/dns-query";
+        String query = "google.com.";
+        DohResolver dohResolver = new DohResolver(host);
+        System.out.println(dohResolver.queryIP(query));
     }
 
 }
