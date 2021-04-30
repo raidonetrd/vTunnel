2549d57f7a425369ae59bff83f8af7a493452841
Alex Tsai <alex.tsai@protonmail.com>
2021-04-30 14:36:30 +0800

    clean

--- app/src/main/java/com/netbyte/vtun/utils/ByteUtil.java
+++ app/src/main/java/com/netbyte/vtun/utils/ByteUtil.java
@@ -9,26 +9,26 @@ public class ByteUtil {
         if (kiloByte < 1) {
             return size + "Byte";
         }
-
+        BigDecimal result;
         double megaByte = kiloByte / 1024;
         if (megaByte < 1) {
-            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
-            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+            result = new BigDecimal(Double.toString(kiloByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
         }
 
         double gigaByte = megaByte / 1024;
         if (gigaByte < 1) {
-            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
-            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+            result = new BigDecimal(Double.toString(megaByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
         }
 
         double teraBytes = gigaByte / 1024;
         if (teraBytes < 1) {
-            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
-            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+            result = new BigDecimal(Double.toString(gigaByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
         }
-        BigDecimal result4 = new BigDecimal(teraBytes);
-        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
+        result = new BigDecimal(teraBytes);
+        return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
     }
 
 }
