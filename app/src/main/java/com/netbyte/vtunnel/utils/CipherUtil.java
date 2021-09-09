26ec678826b27cb94fa651bb229f5228fef31a7d
kucai <kucai@myweimai.com>
2021-09-09 17:32:18 +0800

    update xor

--- app/src/main/java/com/netbyte/vtunnel/utils/CipherUtil.java
+++ app/src/main/java/com/netbyte/vtunnel/utils/CipherUtil.java
@@ -1,63 +1,21 @@
 package com.netbyte.vtunnel.utils;
 
+import java.nio.charset.StandardCharsets;
+
 public class CipherUtil {
-    private static String key = "8pUsXuZw4z6B9EhGdKgNjQnjmVsYv2x5";
+    private static byte [] keys = "8pUsXuZw4z6B9EhGdKgNjQnjmVsYv2x5".getBytes(StandardCharsets.UTF_8);
 
     public CipherUtil(String key) {
-        this.key = key;
+        this.keys = key.getBytes(StandardCharsets.UTF_8);
     }
 
-    public byte[] xor(byte[] data) {
-        RC4 rc4 = new RC4(key.getBytes());
-        return rc4.xor(data);
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    static class RC4 {
-        final byte[] S = new byte[256];
-        final byte[] T = new byte[256];
-        final int keyLength;
-
-        public RC4(final byte[] key) {
-            if (key.length < 1 || key.length > 256) {
-                throw new IllegalArgumentException(
-                        "key must be between 1 and 256 bytes");
-            } else {
-                keyLength = key.length;
-                for (int i = 0; i < 256; i++) {
-                    S[i] = (byte) i;
-                    T[i] = key[i % keyLength];
-                }
-                int j = 0;
-                byte tmp;
-                for (int i = 0; i < 256; i++) {
-                    j = (j + S[i] + T[i]) & 0xFF;
-                    tmp = S[j];
-                    S[j] = S[i];
-                    S[i] = tmp;
-                }
-            }
+    public byte[] xor(byte[] src) {
+        int keyLength = keys.length;
+        for (int i = 0; i < src.length; i++) {
+            src[i] ^= keys[i%keyLength];
         }
+        return src;
 
-        public byte[] xor(final byte[] plaintext) {
-            final byte[] cipherText = new byte[plaintext.length];
-            int i = 0, j = 0, k, t;
-            byte tmp;
-            for (int counter = 0; counter < plaintext.length; counter++) {
-                i = (i + 1) & 0xFF;
-                j = (j + S[i]) & 0xFF;
-                tmp = S[j];
-                S[j] = S[i];
-                S[i] = tmp;
-                t = (S[i] + S[j]) & 0xFF;
-                k = S[t];
-                cipherText[counter] = (byte) (plaintext[counter] ^ k);
-            }
-            return cipherText;
-        }
     }
 
 }
