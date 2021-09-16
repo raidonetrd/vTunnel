26b53e9d654a4a9c6e0b77d59344624cbc3dce7c
netbyte <alex.tsai@protonmail.com>
2021-09-16 13:25:29 +0800

    upgrade 1.3.8

--- app/src/main/java/com/netbyte/vtunnel/thread/VPNThread.java
+++ app/src/main/java/com/netbyte/vtunnel/thread/VPNThread.java
@@ -1,6 +1,5 @@
 package com.netbyte.vtunnel.thread;
 
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
@@ -14,14 +13,9 @@ import com.netbyte.vtunnel.model.LocalIP;
 import com.netbyte.vtunnel.service.IPService;
 import com.netbyte.vtunnel.service.SimpleVPNService;
 import com.netbyte.vtunnel.utils.CipherUtil;
-import com.netbyte.vtunnel.utils.HttpUtil;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
-import java.util.List;
+
 
 public class VPNThread extends Thread {
     private static final String TAG = "VpnThread";
@@ -46,7 +40,13 @@ public class VPNThread extends Thread {
                 .setConfigureIntent(null)
                 .allowFamily(OsConstants.AF_INET)
                 .setBlocking(true);
-        for (String packageName : bypassApps()) {
+        ArrayList<String> appList = new ArrayList<>();
+        appList.add(AppConst.APP_PACKAGE_NAME);
+        if (!TextUtils.isEmpty(config.getBypassApps())) {
+            appList.addAll(Arrays.asList(config.getBypassApps().split(",")));
+        }
+        Log.i(TAG, "bypass apps:" + appList);
+        for (String packageName : appList) {
             builder.addDisallowedApplication(packageName);
         }
         tunnel = builder.establish();
@@ -61,21 +61,4 @@ public class VPNThread extends Thread {
         return this.RUNNING;
     }
 
-    private List<String> bypassApps() {
-        Log.i(TAG, "bypassApps:" + config.getBypassApps());
-        ArrayList<String> result = new ArrayList<>();
-        result.add(AppConst.APP_PACKAGE_NAME);
-        if (TextUtils.isEmpty(config.getBypassApps())) {
-            return result;
-        }
-        List<PackageInfo> packageInfoList = vpnService.getApplicationContext().getPackageManager().getInstalledPackages(0);
-        for (PackageInfo info : packageInfoList) {
-            String packageName = info.packageName;
-            if (config.getBypassApps().contains(packageName)) {
-                result.add(packageName);
-            }
-        }
-        Log.i(TAG, "bypass apps:" + result);
-        return result;
-    }
 }
