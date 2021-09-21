7d5efced1060f73a4ec38fdc56ce01d56857d147
alex <alex@ubuntu.lan>
2021-09-21 13:57:50 +0800

    add app icon

--- app/src/main/java/com/netbyte/vtunnel/model/App.java
+++ app/src/main/java/com/netbyte/vtunnel/model/App.java
@@ -1,16 +1,28 @@
 package com.netbyte.vtunnel.model;
 
+import android.graphics.drawable.Drawable;
+
 import java.io.Serializable;
 
 public class App implements Serializable {
+    private Drawable icon;
     private String name;
     private String packageName;
     private boolean bypass;
 
-    public App(String name, String packageName, boolean bypass) {
+    public App(Drawable icon,String name, String packageName, boolean bypass) {
+        this.icon = icon;
         this.name = name;
         this.packageName = packageName;
         this.bypass = bypass;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Drawable icon) {
+        this.icon = icon;
     }
 
     public String getName() {
