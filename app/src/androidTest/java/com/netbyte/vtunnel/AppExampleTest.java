4d43eaaf3def43f2942d4f4f2110b82e60ec5e56
netbyte <alex.tsai@protonmail.com>
2021-11-30 11:01:33 +0800

    clean test

--- app/src/androidTest/java/com/netbyte/vtunnel/AppExampleTest.java
+++ app/src/androidTest/java/com/netbyte/vtunnel/AppExampleTest.java
@@ -1,23 +1,14 @@
 package com.netbyte.vtunnel;
 
-import android.content.Context;
 
-
-import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
-
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 
 @RunWith(AndroidJUnit4.class)
 public class AppExampleTest {
     @org.junit.Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.netbyte.neturbo", appContext.getPackageName());
     }
 }
