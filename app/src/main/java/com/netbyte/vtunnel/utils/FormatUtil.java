5d8036f032e526122c86fad1e22f625108cdaf1c
Alex <alex.tsai@protonmail.com>
2021-12-04 00:12:36 +0800

    update ui

--- app/src/main/java/com/netbyte/vtunnel/utils/FormatUtil.java
+++ app/src/main/java/com/netbyte/vtunnel/utils/FormatUtil.java
@@ -7,28 +7,28 @@ public class FormatUtil {
     public static String formatByte(long size) {
         double kiloByte = Long.valueOf(size).doubleValue() / 1024;
         if (kiloByte < 1) {
-            return size + "Byte";
+            return size + " Byte";
         }
         BigDecimal result;
         double megaByte = kiloByte / 1024;
         if (megaByte < 1) {
             result = new BigDecimal(Double.toString(kiloByte));
-            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " KB";
         }
 
         double gigaByte = megaByte / 1024;
         if (gigaByte < 1) {
             result = new BigDecimal(Double.toString(megaByte));
-            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " MB";
         }
 
         double teraBytes = gigaByte / 1024;
         if (teraBytes < 1) {
             result = new BigDecimal(Double.toString(gigaByte));
-            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " GB";
         }
         result = new BigDecimal(teraBytes);
-        return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
+        return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " TB";
     }
 
     public static String formatTime(long second) {
